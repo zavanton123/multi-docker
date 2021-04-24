ca0187ce41f31be8009b88914704d9d89c9d2b95
zavanton <zavanton123@gmail.com>
2021-04-24 15:56:01 +0300

    docker updated

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,7 @@ function App() {
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
+        <p>hello world</p>
         <div>
           <Route exact path="/" component={Fib} />
           <Route path="/otherpage" component={OtherPage} />
